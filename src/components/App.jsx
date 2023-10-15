cf4befc74a3708ba40666f4add4c775772a908f7
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-10-15 22:00:47 +0100

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,127 +1,72 @@
-import { Component } from 'react';
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactsList/ContactList";
-import { Filter } from "./Filter/Filter";
-import { MainTitle, SecondaryTitle } from './App.styled'
+import { useEffect, useState } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactsList/ContactList';
+import { Filter } from './Filter/Filter';
+import { MainTitle, SecondaryTitle } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Dimon Oleksenko', number: '666-66-66' },
-      { id: 'id-6', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-7', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-8', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-9', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-10', name: 'Dimon Oleksenko', number: '666-66-66' },
-    ],
-    filter: '',
+export const App = () => {
+  // Функція початкового стану localStorage
+  const initialLocalStorageValue = () => {
+    const contactsData = localStorage.getItem('contacts');
+    if (contactsData !== null) {
+      return JSON.parse(contactsData);
+    }
+    return [];
   };
-// Додаємо новий контакт
-  handleAddContact = newContact => {
-    const enteredName = newContact.name;
 
+  // Хуки
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(initialLocalStorageValue);
+
+  // Функція додавання контакту
+  const handleAddContact = newContact => {
+    const enteredName = newContact.name;
     if (
-      // перевіряємо наявність contact в state.contacts прирівнюючи їх до нижнього регістру
-      this.state.contacts.some(
+      contacts.some(
         contact => contact.name.toLowerCase() === enteredName.toLowerCase()
       )
-    ) 
-    // якщо імя вже є в state.contacts то виводимо alert
-    {
-      alert(`${enteredName} already recorded in the directory`);
-      return;
+    ) {
+      alert(`${enteredName} вже є в каталозі`);
+    } else {
+      setContacts(prevContacts => [...prevContacts, newContact]);
     }
-// Якщо ім'я ще немає в state.contacts то записуємо його до state.contacts збережених
-// у попередньому стані за допомогою prevState.contacts та розпилення масиву (...prevState.contacts)
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, newContact] };
+  };
+  // Видалення контактів
+  const handleContactDelete = contactId => {
+    setContacts(prevState => {
+      return prevState.filter(contact => contact.id !== contactId);
     });
   };
-// Фільтр контактів
-  handleChangeFilter = newSymbol => {
-    this.setState({
-      filter: newSymbol,
-    });
+
+  // Додавання фільтру в стейт
+  const handleChangeFilter = newSymbol => {
+    setFilter(newSymbol);
   };
-// Видалення контакту
-  handleContactDelete = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
-  };
-// Виносимо логіку з рендера
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+
+  // Відфільтровані контакти зі стейту
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
-    )
-  }
-
-// отримуємо contactsData з localStorage.getItem('contacts')
-// парсимо їх JSON.parse(contactsData) та отримуємо parsedContactsData
-// якщо вони - (parsedContactsData) true, то записуємо їх в стейт - this.setState({ contacts: parsedContactsData })
-
-  componentDidMount() {
-    const contactsData = localStorage.getItem('contacts');
-    const parsedContactsData = JSON.parse(contactsData);
-    if (parsedContactsData) {
-      this.setState({ contacts: parsedContactsData });
-    }
-  }
-
-// якщо state не такий як prevState то зберігаємо в localStorage state у вигляді рядка JSON.stringify
-componentDidUpdate(prevProps, prevState) {
-  if (this.state.contacts !== prevState.contacts) {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-
-    return (
-      <div>
-        <MainTitle>Phonebook</MainTitle>
-        <ContactForm addContact={this.handleAddContact} />
-
-        <SecondaryTitle>Contacts</SecondaryTitle>
-        <Filter contactFilter={filter} onChange={this.handleChangeFilter} />
-        <ContactList
-          contacts={filteredContacts}
-          deleteContact={this.handleContactDelete}
-        />
-      </div>
     );
-  }
-}
+  };
 
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
+  return (
+    <div>
+      <MainTitle>Phonebook</MainTitle>
+      <ContactForm addContact={handleAddContact} />
 
+      <SecondaryTitle>Contacts</SecondaryTitle>
+      <Filter contactFilter={filter} onChange={handleChangeFilter} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        deleteContact={handleContactDelete}
+      />
+    </div>
+  );
+};
 
-
+export default App;
